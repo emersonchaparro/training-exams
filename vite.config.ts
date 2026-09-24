af6c1860e61235e9bf54598729f00fc3c18389eb
emersonchaparro <emersonchaparroampa@gmail.com>
2026-09-24 18:40:44 -0500

    feat/examen-git

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/manu-exams/', 
+  base: '/training-exams/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
